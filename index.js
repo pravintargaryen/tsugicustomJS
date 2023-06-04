f984833148217a45e99f1208d14080f53d86c422
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2023-06-04 13:21:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 import data from './quiz.json' assert {type: 'json'}
 
+const saveButton = document.querySelector("#save-button")
+
+window.addEventListener("load", (event) => {
+    saveButton.style.display= 'none'
+  })
+
 const paragraph = document.querySelector("#question")
 const editButton = document.querySelector("#edit-button")
 
-paragraph.innerHTML = `<p>${data.Text}</p>
-<ol>
-<li>${data.Option[0]}</li>
-<li>${data.Option[1]}</li>
-</ol>`
+if (localStorage.getItem("quiz") === null) {
+    paragraph.innerHTML = `<p>${data.Text}</p>
+    <ol>
+    <li>${data.Option[0]}</li>
+    <li>${data.Option[1]}</li>
+    </ol>`
+} else {
+    const data = JSON.parse(localStorage.getItem("quiz"))
+    paragraph.innerHTML = `<p>${data.Text}</p>
+    <ol>
+    <li>${data.Option[0]}</li>
+    <li>${data.Option[1]}</li>
+    </ol>`
+ 
+}
+
 
 editButton.addEventListener("click", function () {
-    paragraph.innerHTML = `
-    <p>Question Text:</p>
-    <textarea id="qtitle" name="qtitle" rows="4" cols="80">${data.Text}</textarea>
-    <p>Option 1:</p><textarea id="option1" name="option1" rows="2" cols="60">${data.Option[0]}</textarea>
-    <p>Option 2:</p><textarea id="option2" name="option2" rows="2" cols="60">${data.Option[1]}</textarea>`
+    editButton.style.display= 'none'
+    saveButton.style.display= 'block'
+    if (localStorage.getItem("quiz") === null) {
+        paragraph.innerHTML = `
+        <p>Question Text:</p>
+        <textarea id="qtitle" name="qtitle" rows="4" cols="80" required>${data.Text}</textarea>
+        <p>Option 1:</p><textarea id="option1" name="option1" rows="2" cols="60" required>${data.Option[0]}</textarea>
+        <p>Option 2:</p><textarea id="option2" name="option2" rows="2" cols="60" required>${data.Option[1]}</textarea>`
+    } else {
+        const data = JSON.parse(localStorage.getItem("quiz"))
+        paragraph.innerHTML = `
+        <p>Question Text:</p>
+        <textarea id="qtitle" name="qtitle" rows="4" cols="80" required>${data.Text}</textarea>
+        <p>Option 1:</p><textarea id="option1" name="option1" rows="2" cols="60" required>${data.Option[0]}</textarea>
+        <p>Option 2:</p><textarea id="option2" name="option2" rows="2" cols="60" required>${data.Option[1]}</textarea>`
 
-    editButton.innerHTML = `<span id="save-button">Save</span>`
+    }
 
 })
 
-const saveButton = document.querySelector("#save-button")
+
 saveButton.addEventListener("click", function() {
     const editQuestion = document.querySelector("#qtitle")
     const editOption1 = document.querySelector("#option1")
     const editOption2 = document.querySelector("#option2")
-
-    data.Text = editQuestion.value
-    data.Option[0] = editOption1.value
-    data.Option[1] = editOption2.value
-
+        data["Text"] = editQuestion.value
+        data.Option[0] = editOption1.value
+        data.Option[1] = editOption2.value
+        localStorage.setItem("quiz", JSON.stringify(data))
+        location.replace("index.html")
+        
 })
 
